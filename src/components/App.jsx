85e403c903abbaf5b6c15b715c8891a66b72f836
Roman <roman.f.grusha@gmail.com>
2022-05-22 17:14:13 +0300

    upd App

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import { Section } from './Section';
+import { Container } from './Container';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+import { HomePage } from '../views/HomePage';
+import { MoviesPage } from '../views/MoviesPage';
+import { MovieDetailsPage } from '../views/MovieDetailsPage';
+import { Cast } from '../views/MovieDetailsPage/Cast';
+import { Reviews } from '../views/MovieDetailsPage/Reviews/';
+
+const App = () => (
+  // <Section>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="movies" element={<MoviesPage />}></Route>
+      <Route path=":movieId/*" element={<MovieDetailsPage />}>
+        <Route path=":cast" element={<Cast />} />
+        <Route path=":reviews" element={<Reviews />} />
+      </Route>
+    </Route>
+  </Routes>
+  // </Section>
+);
+
+export default App;
